0471d8e14c723aeb165041e1e73e4fcd7d080152
FACI <agustincola54@gmail.com>
2025-07-20 16:46:40 -0300

    andate a la puta que te pario java

---
+++ src/main/java/com/example/auth0UsersAndRoles/entities/Character.java
@@ -0,0 +1,25 @@
+package com.example.auth0UsersAndRoles.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Character {
+    @Id
+    private UUID id;
+
+    private String externalId;
+    private String name;
+    private String height;
+    private String mass;
+    private String hairColor;
+    private String skinColor;
+    private String eyeColor;
+    private String birthYear;
+    private String gender;
+    private String homeworld;
+    private String url;
+}
