85684efdff5169a4e5653fff24a64bd3a7e9b668
FACI <agustincola54@gmail.com>
2025-07-23 22:46:47 -0300

    avoid free users to see star wars charachters

---
+++ src/main/java/com/example/auth0UsersAndRoles/config/security/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.auth0UsersAndRoles.config.security;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Map<String, String>> handleAccessDeniedException(AccessDeniedException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "action_not_available_only_for_premium_users");
+        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+    }
+}
--- src/main/java/com/example/auth0UsersAndRoles/controlers/StarWarsController.java
+++ src/main/java/com/example/auth0UsersAndRoles/controlers/StarWarsController.java
@@ -3,6 +3,7 @@ package com.example.auth0UsersAndRoles.controlers;
 import com.example.auth0UsersAndRoles.services.StarWarsService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class StarWarsController {
 
     @Operation(summary = "Get all star wars characters", description = "Fetches characters from http://localhost:3000/personajes")
     @GetMapping("/personajes")
+    @PreAuthorize("hasAuthority('premium')")
     public ResponseEntity<String> getCharacters() {
         try {
             String response = starWarsService.getCharacters();
